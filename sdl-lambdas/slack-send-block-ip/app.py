de94d9ec5c7fa5ba55f96afa76afc6635fa102c9
DEBOJIT ROY <debojity2k@gmail.com>
2023-11-10 12:46:51 +0530

    Added slack-send-block-ip lambda

---
+++ sdl-lambdas/slack-send-block-ip/app.py
@@ -0,0 +1,81 @@
+import json
+import os
+import requests
+
+slack_message = '''
+{
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*New IP Blocking Request - [IP]*"
+			}
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": "*Type:*\\nNACL Block"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "*Source:*\\nGuard Duty"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "*Reason:*\\nBrute Force Attack"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "*IPv4:*\\n*[IP]*"
+				}
+			]
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": "Approve"
+					},
+					"style": "primary",
+					"value": "[task_token]#approve"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"emoji": true,
+						"text": "Deny"
+					},
+					"style": "danger",
+					"value": "[task_token]#deny"
+				}
+			]
+		}
+	]
+}
+'''
+
+
+def lambda_handler(event, context):
+    ip = event["ip_to_block"]
+    task_token = event["task_token"]
+    slack_url = os.environ["slack_url"]
+
+    message_body_str = slack_message.replace("[IP]", ip).replace("[task_token]", task_token)
+
+    message_body = json.loads(message_body_str)
+    print(message_body)
+
+    response = requests.post(slack_url, json=message_body)
+    print(response)
+
+    return {
+        'statusCode': 200
+    }
